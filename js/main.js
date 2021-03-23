5c178460119732c76c8ec26a3ad1186fed8e92e6
MEvgen <merginskiy@gmail.com>
2021-03-23 17:11:10 +0200

    first day

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const mySwiper = new Swiper(".swiper-container", {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev"
+  }
+});
+
+// Cart
+const buttonCart = document.querySelector(".button-cart");
+const modalCart = document.querySelector("#modal-cart");
+const modalClose = modalCart.querySelector(".modal-close");
+
+const openModal = () => {
+  modalCart.classList.add("show");
+};
+const closeModal = () => {
+  modalCart.classList.remove("show");
+};
+
+buttonCart.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
+
+// Cart DZ
+modalCart.addEventListener("click", event => {
+  let target = event.target;
+  if (target.classList.contains("overlay")) {
+    closeModal();
+  }
+});
+
+// Scroll smooth
+const scrollLinks = document.querySelectorAll("a.scroll-link");
+for (i = 0; i < scrollLinks.length; i++) {
+  console.log(scrollLinks[i]);
+  console.log(scrollLinks[i].getAttribute("href"));
+}
+
+scrollLinks.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    // const target = event.target;
+    const id = link.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
+  });
+});
+// console.log(topLinks);
